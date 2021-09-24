82387fd24aff1b8168bdb003feb651166738043f
ms919 <sakuraimiki919@gmail.com>
2021-09-24 09:50:37 +0900

    Fix: Topページ、デフォルト修正

--- components/Top.vue
+++ components/Top.vue
@@ -9,7 +9,7 @@
 			</p>
 		</div>
 		<div class="buttons">
-      <NuxtLink type="button" to="/play" class="button btn-pink">Play</NuxtLink>
+			<NuxtLink type="button" to="/play" class="button btn-pink">Play</NuxtLink>
 			<a href="#">Login with</a>
 		</div>
 	</div>
@@ -20,7 +20,6 @@
 	position: absolute;
 	left: 15%;
 	top: 28%;
-	/* background-color: white; */
 }
 span {
 	text-decoration: underline wavy 0.3rem #007acc;
@@ -30,35 +29,7 @@ span {
 	position: absolute;
 	left: 60%;
 	top: 49%;
-  width: 20%;
-  text-align: center;
-	/* background-color: palegreen; */
-}
-.button {
-	display: inline-block;
+	width: 20%;
 	text-align: center;
-	width: 100%;
-	vertical-align: middle;
-	-webkit-user-select: none;
-	-moz-user-select: none;
-	-ms-user-select: none;
-	user-select: none;
-	box-sizing: border-box;
-	padding: 0.375rem 0.75rem;
-  margin-bottom: 2rem;
-	font-size: 3rem;
-	line-height: 1.5;
-	border-radius: 0.8rem;
-	transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
-		border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-	transition-property: color, background-color, border-color, box-shadow;
-	transition-duration: 0.15s, 0.15s, 0.15s, 0.15s;
-	transition-timing-function: ease-in-out, ease-in-out, ease-in-out, ease-in-out;
-	transition-delay: 0s, 0s, 0s, 0s;
-}
-.btn-pink {
-	color: #c586c0;
-	background: #3a3d41;
-	border: 5px solid #c586c0;
 }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -23,7 +23,62 @@ p {
 	font-size: 3rem;
 }
 a {
-  font-size: 2.5rem;
-  color: #d4d4d4;
+	font-size: 2.5rem;
+	color: #d4d4d4;
+}
+.button {
+	display: inline-block;
+	text-align: center;
+	vertical-align: middle;
+	-webkit-user-select: none;
+	-moz-user-select: none;
+	-ms-user-select: none;
+	user-select: none;
+	box-sizing: border-box;
+	padding: 0.375rem 7rem;
+	margin-bottom: 2rem;
+	font-size: 3rem;
+	line-height: 1.5;
+	border-radius: 0.8rem;
+	transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+		border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+	transition-property: color, background-color, border-color, box-shadow;
+	transition-duration: 0.15s, 0.15s, 0.15s, 0.15s;
+	transition-timing-function: ease-in-out, ease-in-out, ease-in-out, ease-in-out;
+	transition-delay: 0s, 0s, 0s, 0s;
+}
+.btn-pink {
+	color: #c586c0;
+	background: #3a3d41;
+	border: 5px solid #c586c0;
+}
+.quiz-p {
+	line-height: 1em;
+	cursor: pointer;
+	margin-right: 1rem;
+}
+.orange {
+	color: #ce9178;
+}
+.skyblue {
+	color: #9cdcfe;
+}
+.lightblue {
+	color: #569cd6;
+}
+.green {
+	color: #6a9955;
+}
+.pink {
+	color: #c586c0;
+}
+.yellow {
+	color: #dcdcaa;
+}
+.cleargreen {
+	color: #4ec9b0;
+}
+.blue {
+	color: #007acc;
 }
 </style>
